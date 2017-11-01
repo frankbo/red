c4e228172fb79a977381478d6cde5d46f9e37134
Frank Boeddeker <frank.boeddeker@gmx.net>
2017-11-01 16:57:39 +0100

    Use foldRight for sum and prod

--- src/main/scala/Chapters/Chapter3.scala
+++ src/main/scala/Chapters/Chapter3.scala
@@ -69,4 +69,14 @@ object Chapter3 {
 
     go(l, List())
   }
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]): Int = foldRight(ns, 0)(_ + _)
+
+  def product2(ns: List[Double]): Double = foldRight(ns, 1.0)(_ * _)
 }
