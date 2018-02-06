e99ef8d2b73923ad39afd1732c2e455c2511df0e
Frank <frank.boeddeker@gmx.net>
2018-02-07 00:00:48 +0100

    Add toList for Streams

--- src/main/scala/Chapters/Chapter5/Stream.scala
+++ src/main/scala/Chapters/Chapter5/Stream.scala
@@ -1,8 +1,18 @@
 package Chapters.Chapter5
 
-sealed trait Stream[+A]
-case object Empty extends Stream[Nothing]
-case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+sealed trait Stream[+A] {
+  def toList: List[A]
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, _) => Some(h())
+  }
+}
+case object Empty extends Stream[Nothing] {
+  override def toList = List()
+}
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A] {
+  override def toList = h() :: t().toList
+}
 
 object Stream {
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
--- src/test/scala/Chapters/Chapter5/StreamSpec.scala
+++ src/test/scala/Chapters/Chapter5/StreamSpec.scala
@@ -3,5 +3,11 @@ package Chapters.Chapter5
 import org.scalatest.{FlatSpec, Matchers}
 
 class StreamTest extends FlatSpec with Matchers {
+  "toList" should "convert an empty Stream into an empty list" in {
+    Stream().toList shouldEqual List()
+  }
 
+  it should "convert a stream into a list" in {
+    Stream(1, 2, 3).toList shouldEqual List(1, 2, 3)
+  }
 }
