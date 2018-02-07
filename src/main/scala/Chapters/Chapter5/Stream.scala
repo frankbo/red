b8565243e23fa69809dd693bd0f2076bf650569e
Frank <frank.boeddeker@gmx.net>
2018-02-07 01:04:16 +0100

    Take and drop for stream. Take implementation is missing

--- src/main/scala/Chapters/Chapter5/Stream.scala
+++ src/main/scala/Chapters/Chapter5/Stream.scala
@@ -2,6 +2,8 @@ package Chapters.Chapter5
 
 sealed trait Stream[+A] {
   def toList: List[A]
+  def take(n: Int): Stream[A]
+  def drop(n: Int): Stream[A]
   def headOption: Option[A] = this match {
     case Empty => None
     case Cons(h, _) => Some(h())
@@ -9,9 +11,13 @@ sealed trait Stream[+A] {
 }
 case object Empty extends Stream[Nothing] {
   override def toList = List()
+  override def take(n: Int) = Stream()
+  override def drop(n: Int) = Stream()
 }
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A] {
   override def toList = h() :: t().toList
+  override def take(n: Int) = ???
+  override def drop(n: Int) = if (n == 0) t() else t().drop(n - 1)
 }
 
 object Stream {
--- src/test/scala/Chapters/Chapter5/StreamSpec.scala
+++ src/test/scala/Chapters/Chapter5/StreamSpec.scala
@@ -10,4 +10,20 @@ class StreamTest extends FlatSpec with Matchers {
   it should "convert a stream into a list" in {
     Stream(1, 2, 3).toList shouldEqual List(1, 2, 3)
   }
+
+  "take" should "return an empty stream when take can not get anything" in {
+    Stream().take(1) shouldEqual Stream()
+  }
+
+  it should "return the first n elements from the stream" in {
+    Stream(1,2,3,4,5,6).take(1).toList shouldEqual Stream(1,2).toList
+  }
+
+  "drop" should "return an empty stream when there is nothing to drop" in {
+    Stream().drop(1) shouldEqual Stream()
+  }
+
+  it should "return the remaining stream when the n elements are dropped" in {
+    Stream(1, 2, 3, 4).drop(1).toList shouldEqual Stream(3, 4).toList // Why is toList relevant here?
+  }
 }
