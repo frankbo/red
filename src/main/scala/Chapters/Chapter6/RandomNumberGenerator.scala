ca8bc25b00634d1b4609f97c697109db161a5373
Frank <frank.boeddeker@gmx.net>
2018-02-22 00:06:29 +0100

    Add nonNegativeInt to RNG

--- src/main/scala/Chapters/Chapter6/RandomNumberGenerator.scala
+++ src/main/scala/Chapters/Chapter6/RandomNumberGenerator.scala
@@ -2,4 +2,21 @@ package Chapters.Chapter6
 
 trait RNG {
   def nextInt: (Int, RNG)
+
+  def nonNegativeInt(rng: RNG): (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+
+  // Todo Catch Int.MinValue error and find out how to write a test for it.
+  override def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (v1, n1) = rng.nextInt
+    if (v1 >= 0) (v1, n1) else nonNegativeInt(n1)
+  }
 }
--- src/test/scala/Chapters/Chapter6/RandomNumberGeneratorSpec.scala
+++ src/test/scala/Chapters/Chapter6/RandomNumberGeneratorSpec.scala
@@ -4,5 +4,9 @@ package Chapters.Chapter6
 import org.scalatest.{FlatSpec, Matchers}
 
 class RandomNumberGeneratorSpec extends FlatSpec with Matchers {
-
+    "nonNegativeInt" should "always return a positive number" in {
+      val rng = SimpleRNG(123)
+      val (_, rng2) = rng.nextInt
+      rng.nonNegativeInt(rng2)._1 should be > 0
+    }
 }
